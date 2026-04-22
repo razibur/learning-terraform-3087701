c98d049a15605576c635276be64a61e3ef6b20dd
razibur <razibur@outlook.com>
2026-04-22 11:35:56 -0400

    Create security group

--- main.tf
+++ main.tf
@@ -18,7 +18,35 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  vpc_security_gropup_ids = [aws_security_group.id]
+
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "blog" {
+  name = "blog"
+  description = "Allow http and https in. Allow everything out"   
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type = "ingress"
+  from_port - 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_http_out" {
+  type = "egress"
+  from_port - 80
+  to_port = 80
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
 }
